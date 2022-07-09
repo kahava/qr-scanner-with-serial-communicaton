c31da759cac18d0c6e5b6abf1d721ae529e21285
kahava <kahavadesigner@gmail.com>
2022-07-09 14:29:53 -0700

    complete qr scanner for proteus simulation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import {Container, Card, CardContent, makeStyles, Grid} from '@material-ui/core';
-
 import QrReader from 'react-qr-reader';
-
-
-
 
 
 function App() { 
 
+  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const classes = useStyles();
+ 
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  }
+  const handleScanWebCam = (result) => {
+    if (result){
+        setScanResultWebCam(result);
+    }
+   }
 
-  function SendData(){
+   useEffect(()=>{
 
     const data = {
-      name: "anna", age:24
-    };
-    
-    useEffect(()=>{
+      paid:`${scanResultWebCam}`
+    }
+      
     fetch("/home", {
       method:"POST",
       headers: {
@@ -28,40 +34,17 @@ function App() {
     ).then(res => res.json()).then(info => console.log(info))
     
     })
-    
-    }
-    
-    
-
-
-
-
-
- 
-  const [scanResultWebCam, setScanResultWebCam] =  useState('');
-  const classes = useStyles();
- 
-  const handleErrorWebCam = (error) => {
-    console.log(error);
-  }
-  const handleScanWebCam = (result) => {
-    if (result){
-        setScanResultWebCam(result);
-
-    }
-
-   }
-     console.log(scanResultWebCam)
   return (
     <Container className={classes.conatiner}>
           <Card>
-            <button onClick={SendData()}>click</button>
+            
               
               <CardContent>
                   <Grid container spacing={2}>
                      
                       <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                         <h3>Scan QR code to verify payments</h3>
+                         <h1>Scan QR code to verify payments @grace juice</h1>
+                         <h2 style={{color:"green"}} >Paid Juice: {scanResultWebCam}</h2>
                          <QrReader
                          delay={300}
                          style={{width: '100%'}}
