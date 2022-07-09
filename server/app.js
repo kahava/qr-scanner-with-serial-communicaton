e9bb1b4ab722eca0eeb3e80312e0d64a5a2086d9
kahava <kahavadesigner@gmail.com>
2022-07-09 14:28:22 -0700

    complete qr scanner for proteus simulation

--- server/app.js
+++ server/app.js
@@ -1,24 +1,56 @@
 import  express  from "express";
 import bodyParser from "body-parser";
-
+import cors from "cors";
+import { SerialPort } from 'serialport';
+import { ReadlineParser } from '@serialport/parser-readline';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const port = 1212;
 
-app.get('/home', (req, res)=>{
-res.json({name:"adam", age:12});
+
+// CONNECTION TO SERIAL PORT start
+const parser = new ReadlineParser({ delimiter: '\r\n' });
+
+try{
+    const proteusPort = new SerialPort({ path: 'COM5',
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
 });
 
+proteusPort.pipe(parser);
+}
+
+catch(err){
+    console.log("Please configure virtual serial port correct");
+}
+// CONNECTION TO SERIAL PORT end
+
+
+// accepting data from the scanning camera
 app.post("/home", (req, res)=>{
-    console.log(req.body);
+   const {paid} = req.body;
+   try {
+    console.log("paid juice type:",paid);
+
+    //sending data to serial port
+    proteusPort.write(paid);
+
+   } catch (error) {
+
+    console.log("Please configure correct virtual port in your simulation. It seems the port does not receive sent data")
+   }
 })
 
 
 
 app.listen(port, ()=>{
-    console.log(`the app connected at port: ${port}`);
+    console.log(`Grace juice server connected at port: ${port}`);
 })
